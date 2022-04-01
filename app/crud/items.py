b09e5cb6e0a544f9a5facc64675a88a6e55f1191
hyunjin-lim <limhj9230@gmail.com>
2022-04-01 10:31:34 +0900

    test

--- app/crud/items.py
+++ app/crud/items.py
@@ -6,9 +6,6 @@ from app.crud.crud_base import CrudBase
 
 class CrudItem(CrudBase[Item, ItemCreate, ItemUpdate]):
 
-    def get_items(self, db: Session, skip: int = 0, limit: int = 100):
-        return db.query(Item).offset(skip).limit(limit).all()
-
     def create_user_item(self, db: Session, item: ItemCreate, user_id: int):
         db_item = Item(**item.dict(), owner_id=user_id)
         db.add(db_item)
